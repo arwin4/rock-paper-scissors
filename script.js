c9aa1158f245788969618cb89720a6b356a74a45
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-02-02 18:08:39 +0100

    Add function for correct capitalization of Rock, Paper, Scissors

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const computerSelection = getComputerChoice();
 console.log(computerSelection);
 
-let playerSelection = prompt('Rock paper scissors! Enter your choice:');
-playerSelection = playerSelection.toLowerCase();
+let playerSelection = prompt('Rock Paper Scissors! Enter your choice:');
+playerSelection = capitalize(playerSelection)
 console.log(playerSelection);
 
 playRound(playerSelection, computerSelection);
 console.log(playRound(playerSelection, computerSelection));
 
+function capitalize(playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    firstLetter = playerSelection.substring(0, 1);
+    restLetters = playerSelection.substring(1);
+
+    firstLetter = firstLetter.toUpperCase();
+
+    return firstLetter + restLetters;
+}
+
 function getComputerChoice() {
-    const possibleAttacks = ['rock', 'paper', 'scissors'];
+    const possibleAttacks = ['Rock', 'Paper', 'Scissors'];
     const choice = Math.floor(possibleAttacks.length * Math.random());
     return possibleAttacks[choice];
 }
@@ -18,29 +29,17 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return `You both chose ${playerSelection}, so no one wins.`;
     } 
-    else if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
+    // There are only three win conditions, so we only need to check for those.
+    else if ((playerSelection === 'Paper') && (computerSelection === 'Rock')) {
         return `You win! ${playerSelection} beats ${computerSelection}!`;
     }
-    else if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
+    else if ((playerSelection === 'Rock') && (computerSelection === 'Scissors')) {
         return `You win! ${playerSelection} beats ${computerSelection}!`;
     }
-    else if ((playerSelection === 'scissors') && (computerSelection === 'paper')) {
+    else if ((playerSelection === 'Scissors') && (computerSelection === 'Paper')) {
         return `You win! ${playerSelection} beats ${computerSelection}!`;
     }
     else {
         return `You lose! ${computerSelection} beats ${playerSelection}.`;
     }
-    // Ask for player's choice of hand
-    // Convert choice to lowercase string
-    // List which hand beats the others
-        // | if same choice? tie.
-        // | if win condition? --> win
-            // the only win conditions are:
-            // player paper: computer rock
-            // player rock: computer scissors
-            // player scissors: computer paper
-        // | else --> lose
-    // compare hands
-    // if player beats computer, print "you win! playerSelection beats computerSelection"
-    // if player loses, print "you lose! computerSelection beats playerSelection"
 }
