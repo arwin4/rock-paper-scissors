8c4dddcc9812aff379d75dd61c41ee707aba176d
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-02-04 12:42:46 +0100

    Add tiebreaker feature
    - Divide program into more functions
    - Improve function names
    - Add ending message that shows the final score

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-// const computerSelection = getComputerChoice();
-// console.log(computerSelection);
-
-// let playerSelection = prompt('Rock Paper Scissors! Enter your choice:');
-// playerSelection = capitalize(playerSelection)
-// console.log(playerSelection);
-
-// playRound(playerSelection, computerSelection);
-// console.log(playRound(playerSelection, computerSelection));
 let playerWins = 0;
 let computerWins = 0;
+let checkTiebreaker = false;
 
+// play hardcoded number of rounds
 game();
+
+while (computerWins === playerWins) {
+    tieBreaker()
+}
+
+if (playerWins > computerWins) {
+    if (checkTiebreaker === true) {
+        console.log('You won the tiebreaker! Congrats!');
+    } else {
+        console.log('You won the game! Congrats!');
+    }
+    showFinalScore(playerWins, computerWins);
+
+} else {
+    if (checkTiebreaker === true) {
+        console.log('You lost the tiebreaker. Tough luck...');
+    } else {
+        console.log('You lost the game! That\'s a shame...');
+    }
+    showFinalScore(playerWins, computerWins);
+}
 
 function capitalize(playerSelection) {
     playerSelection = playerSelection.toLowerCase();
@@ -35,7 +49,6 @@ function getPlayerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    
     if (playerSelection === computerSelection) {
         return;
     }
@@ -57,36 +70,50 @@ function playRound(playerSelection, computerSelection) {
 function game() {
     for (let i = 0; i < 5; i++) {
         const computerSelection = getComputerChoice();
-        console.log('computer picked ' + computerSelection);
+        // console.log('Debug: computer picked ' + computerSelection);
         const playerSelection = getPlayerChoice();
 
         const checkRoundWin = playRound(playerSelection, computerSelection);
 
-        if (checkRoundWin === true) {
-            playerWins++;
-            console.log(`You win round ${i + 1}! ${playerSelection} beats ${computerSelection}!`);
-        }
-        else if (checkRoundWin === false) {
-            computerWins++;
-            console.log(`You lose round ${i + 1}! ${computerSelection} beats ${playerSelection}!`);
-        }
-        else {
-            console.log(`It's a tie this round (${i + 1})! You both chose ${playerSelection}.`);
-        }
+        showRoundResult(checkRoundWin, i, computerSelection, playerSelection);
     }
-    console.log('playerwins ' + playerWins);
-    console.log('computerwins ' + computerWins);
-    
-  /*
-    while computerwins === playerwins:
-        playRound()
+}
 
+function showRoundResult(checkRoundWin, i, computerSelection, playerSelection) {
+    if (checkRoundWin === true) {
+        playerWins++;
+        console.log(`You win round ${i + 1}! ${playerSelection} beats ${computerSelection}!`);
+    }
+    else if (checkRoundWin === false) {
+        computerWins++;
+        console.log(`You lose round ${i + 1}! ${computerSelection} beats ${playerSelection}!`);
+    }
+    else {
+        console.log(`It's a tie this round (${i + 1})! You both chose ${playerSelection}.`);
+    }
+}
 
-    who won the game?
-    print playerWins and computerWins
-    if player > computer: player win message
-        else if computer > player: player lose message
+function tieBreaker() {
+    checkTiebreaker = true;
+    console.log('We still don\'t have a winner. Let\'s play again.');
 
-        else (equal wins or no wins): tiebreaker:
-    */
+    const computerSelection = getComputerChoice();
+    // console.log('Debug: computer picked ' + computerSelection);
+    const playerSelection = getPlayerChoice();
+
+    const checkRoundWin = playRound(playerSelection, computerSelection);
+
+    if (checkRoundWin === true) {
+        playerWins++;
+        console.log(`${playerSelection} beats ${computerSelection}!`);
+    } else if (checkRoundWin === false) {
+        computerWins++;
+        console.log(`${computerSelection} beats ${playerSelection}!`);
+    } // if it's a tie again (checkRoundWin === undefined), stay in while loop
+}
+
+function showFinalScore(playerWins, computerWins) {
+    console.log(`== Final score ==
+You: ${playerWins} games won
+Computer: ${computerWins} games won`)
 }
